36beb4c3a737d177c05c16f1720740580264f302
akib024 <akib_kuet@yahoo.com>
2023-10-15 23:08:57 +0600

    final commit

--- src/main/java/net/javaguides/departmentservice/DepartmentServiceApplication.java
+++ src/main/java/net/javaguides/departmentservice/DepartmentServiceApplication.java
@@ -2,8 +2,10 @@ package net.javaguides.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
